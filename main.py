d1c3b3016d0ff7ccda763b0e8cfea67ab1245ede
njadnissi <njadnissi@gmail.com>
2023-12-17 20:31:32 +0800

    debug mode put in environment.

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
+import os
 from website import create_app
 
+
+DEBUG = bool(os.getenv("CONF_DEBUG", False))
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True)
-    # app.run(debug=False, host='0.0.0.0')
+    print(DEBUG)
+    app.run(debug=DEBUG, host='0.0.0.0')
